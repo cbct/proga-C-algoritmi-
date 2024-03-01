18940d8e7f180cfe549abb7b8ad215e4290f05e3
qt <39391890+cbct@users.noreply.github.com>
2024-03-01 20:04:40 +1000

    Add files via upload

---
+++ 3 (символы)/simvoli.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void UPER(char *s, int ogr)
+{
+    for(int i= 0; i < ogr; i++){
+        s[i] = toupper((unsigned char)s[i]);
+
+    }
+}
+
+int main()
+{
+
+    
+    int ogr;
+    printf("Vvedite ogranichenie simvolov: ");
+    scanf("%d", &ogr);
+    fflush(stdin);
+
+
+    char *s = (char *)malloc(ogr * sizeof(char));
+
+
+    if (s == NULL) 
+    {
+        fprintf(stderr, "RAM ERROR\n");
+        return 1;
+    }
+
+
+
+
+    printf("Vasha stroca: ");
+    fgets(s, ogr + 1,  stdin);
+    s[ogr] = '\0';
+    printf("Vasha stroca low: %s \n", s);
+    UPER(s, ogr);
+    printf("Vasha stroca Up: %s", s);
+    
+
+    free(s);
+    getchar();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
