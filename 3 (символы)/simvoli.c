1b0cb72a3409f1444e3c7cd00d4a51be693eab4d
qt <39391890+cbct@users.noreply.github.com>
2024-03-09 14:38:53 +1000

    Update simvoli.c

--- 3 (символы)/simvoli.c
+++ 3 (символы)/simvoli.c
@@ -11,17 +11,14 @@ void UPER(char *s, int ogr)
     }
 }
 
-int main()
-{
+int main() {
 
-    
     int ogr;
     printf("Vvedite ogranichenie simvolov: ");
-    scanf("%d", &ogr);
-    fflush(stdin);
-    
-    char *s = (char *)malloc(ogr * sizeof(char));
+    scanf(" %d", &ogr);
+    getchar();
 
+    char *s = (char *)malloc(ogr * sizeof(char));
 
     printf("Vasha stroca: ");
     fgets(s, ogr + 1,  stdin);
@@ -29,60 +26,12 @@ int main()
     printf("Vasha stroca low: %s \n", s);
     UPER(s, ogr);
     printf("Vasha stroca Up: %s", s);
-    
 
     free(s);
     getchar();
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <ctype.h>
-
-// void UPER(char *s, int ogr)
-// {
-//     for(int i= 0; i < ogr; i++){
-//         if(s[i] == 'a' || s[i] == 'b')
-//         s[i] = toupper(s[i]);
-
-//     }
-// }
-
-// int main() {
-
-//     int ogr;
-//     printf("Vvedite ogranichenie simvolov: ");
-//     scanf(" %d", &ogr);
-//     getchar();
-
-//     char *s = (char *)malloc(ogr * sizeof(char));
-
-//     printf("Vasha stroca: ");
-//     fgets(s, ogr + 1,  stdin);
-//     s[ogr] = '\0';
-//     printf("Vasha stroca low: %s \n", s);
-//     UPER(s, ogr);
-//     printf("Vasha stroca Up: %s", s);
-
-//     free(s);
-//     getchar();
-
-//     return 0;
-// }
 
 
 
