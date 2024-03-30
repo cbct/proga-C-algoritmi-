cb9115fd5361592caf4c7c5486d4ef8299029bf6
qt <39391890+cbct@users.noreply.github.com>
2024-03-30 14:42:51 +1000

    Update func.h

--- 5 (калькулятор матриц)/func.h
+++ 5 (калькулятор матриц)/func.h
@@ -1,6 +1,7 @@
 void F_mult(int line_A, int stolb_A, int line_B, int stolb_B, double **matrix_A, double **matrix_B, double **matrix_C);
 void F_minus(int line_A, int stolb_A, int line_B, int stolb_B, double **matrix_A, double **matrix_B, double **matrix_C);
 void F_summa(int line_A, int stolb_A, int line_B, int stolb_B, double **matrix_A, double **matrix_B, double **matrix_C);
+void print_matrix(int line_A, int stolb_A, double **matrix_C);
 
 
 
